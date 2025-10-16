465dc8ae0c76d68f4f468863aa06ec6170ab0262
Deen Mohammad <deen9721@gmail.com>
2025-10-16 12:04:59 +0600

    Create  product DTO

---
+++ Product_Details_Register/Models/ProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Product_Details_Register.Models
+{
+    public class ProductDTO
+    {
+        [Required, MaxLength(100)]
+        public string Name { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Brand { get; set; } = "";
+
+        [Required, MaxLength(100)]
+        public string Category { get; set; } = "";
+
+        [Required]
+        public decimal Price { get; set; }
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = "";
+       
+    }
+}
