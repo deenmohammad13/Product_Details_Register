74d3c3efe657a83f8d7845a5deb25f3441c53598
Deen Mohammad <deen9721@gmail.com>
2025-10-16 12:25:21 +0600

    Create controller and inject Connection string

---
+++ Product_Details_Register/Controllers/ProductsController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Product_Details_Register.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ProductsController : ControllerBase
+    {
+        private readonly string connectionString;
+        public ProductsController(IConfiguration config)
+        {
+            connectionString = config.GetConnectionString("DefaultConnection")!;
+        }
+    }
+}
